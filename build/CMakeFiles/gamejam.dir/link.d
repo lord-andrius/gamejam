f19a8b9718e3be18632d9c638fe342e7e9fa3510
Andrew Dylan <capar2023113TADS0002@aluno.ifpi.edu.br>
2025-07-04 10:12:03 -0300

    primeiro commit

---
+++ build/CMakeFiles/gamejam.dir/link.d
@@ -0,0 +1,52 @@
+Debug/gamejam: \
+ /usr/lib64/Scrt1.o \
+ /usr/lib64/crti.o \
+ /usr/lib64/gcc/x86_64-openmandriva-linux-gnu/14.2.1/crtbeginS.o \
+ CMakeFiles/gamejam.dir/src/main.c.o \
+ /home/infra/Documentos/c/gamejam/vendor/linux/lib/libraylib.a \
+ /usr/lib64/libm.so \
+ /usr/lib64/libm.so.6 \
+ /usr/lib64/libmvec.so.1 \
+ /usr/lib64/gcc/x86_64-openmandriva-linux-gnu/14.2.1/libgcc.a \
+ /usr/lib64/libgcc_s.so \
+ /usr/lib64/libgcc_s.so.1 \
+ /usr/lib64/libc.so \
+ /usr/lib64/libc.so.6 \
+ /usr/lib64/libc_nonshared.a \
+ /usr/lib64/ld-linux-x86-64.so.2 \
+ /usr/lib64/gcc/x86_64-openmandriva-linux-gnu/14.2.1/crtendS.o \
+ /usr/lib64/crtn.o
+
+/usr/lib64/Scrt1.o:
+
+/usr/lib64/crti.o:
+
+/usr/lib64/gcc/x86_64-openmandriva-linux-gnu/14.2.1/crtbeginS.o:
+
+CMakeFiles/gamejam.dir/src/main.c.o:
+
+/home/infra/Documentos/c/gamejam/vendor/linux/lib/libraylib.a:
+
+/usr/lib64/libm.so:
+
+/usr/lib64/libm.so.6:
+
+/usr/lib64/libmvec.so.1:
+
+/usr/lib64/gcc/x86_64-openmandriva-linux-gnu/14.2.1/libgcc.a:
+
+/usr/lib64/libgcc_s.so:
+
+/usr/lib64/libgcc_s.so.1:
+
+/usr/lib64/libc.so:
+
+/usr/lib64/libc.so.6:
+
+/usr/lib64/libc_nonshared.a:
+
+/usr/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib64/gcc/x86_64-openmandriva-linux-gnu/14.2.1/crtendS.o:
+
+/usr/lib64/crtn.o:
